c0b26c347e6cf5b9a5dc398038791a94f72248b3
alejandromp12 <alejandromp12.4@gmail.com>
2021-03-16 20:52:51 -0600

    add struct and basic functions

---
+++ proyect1/src/backend/include/threadsManager.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+typedef enum
+{
+    EXPROPRIATED = 0,
+    NON_EXPROPRIATED = 1
+} OperationModeEnum; ///<
+
+typedef struct
+{
+    int tickets;
+    int workLoad;
+    int quantum;
+    OperationModeEnum mode;
+} *thread; ///<
+
+///
+void populateWorker(void *pThread, int tickets, int workLoad, int quantum, OperationModeEnum mode);
+
+///
+void updateQuantum(void *pThread, int newQuantum);
+
+
--- proyect1/src/backend/src/threadsManager.c
+++ proyect1/src/backend/src/threadsManager.c
@@ -1,3 +1,18 @@
-#include <stdio.h>
-
 #include "include/threadsManager.h"
+
+/**
+ *
+ */
+void populateWorker(void *pThread, int tickets, int workLoad, int quantum, OperationModeEnum mode)
+{
+	thread pWorker = (thread)pThread;
+	if (pWorker)
+	{
+		pWorker->tickets = tickets;
+		pWorker->workLoad = workLoad;
+		pWorker->quantum = quantum;
+		pWorker->mode = mode;
+	}
+}
+
+
